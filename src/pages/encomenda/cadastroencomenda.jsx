d418346ee81a7e59a520ad4fd1735874d15e3882
Alex Raulino <prefeiturapro@gmail.com>
2026-01-07 13:35:39 -0300

    teste novo branch

--- src/pages/encomenda/cadastroencomenda.jsx
+++ src/pages/encomenda/cadastroencomenda.jsx
@@ -3,7 +3,7 @@ import { useNavigate, useLocation } from "react-router-dom";
 import CadastroContribuinte from "../contribuinte/cadastrocontribuinte";
 import { CupomEncomenda } from "../../components/CupomEncomenda"; 
 // alteração
-
+// outro teste
 // --- ÍCONES SVG ---
 const IconCliente = () => <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/></svg>;
 const IconCakeMenu = () => <svg className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 15.546c-.523 0-1.046.151-1.5.454a2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.704 2.704 0 01-3 0 2.704 2.704 0 00-3 0 2.701 2.701 0 00-1.5-.454M9 6v2m3-2v2m3-2v2M9 3h.01M12 3h.01M15 3h.01M21 21v-7a2 2 0 00-2-2H5a2 2 0 00-2 2v7h18zm-3-9v-2a2 2 0 00-2-2H8a2 2 0 00-2 2v2h12z"/></svg>;
